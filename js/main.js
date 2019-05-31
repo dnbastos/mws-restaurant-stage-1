fbf3138ae854d7901574b9b8f614a11f1575c0f1
Daniel Bastos <danielbastos.17@hotmail.com>
2019-05-31 11:41:52 -0300

    Fix problems of semantics and ARIA roles

--- js/main.js
+++ js/main.js
@@ -171,7 +171,7 @@ createRestaurantHTML = (restaurant) => {
   picture.append(image);
   li.append(picture);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
