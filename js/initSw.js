5a7bffe6ab701861fa9ae6bbdf39ada62edbd273
Daniel Bastos <danielbastos.17@hotmail.com>
2019-05-31 11:39:57 -0300

    Fix SW register

--- js/initSw.js
+++ js/initSw.js
@@ -1,7 +1,7 @@
 /**
  * Register Service Worker.
  */
-window.onload = () => {
+(() => {
   if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
       .then(registration => console.log(registration))
@@ -9,4 +9,4 @@ window.onload = () => {
   } else {
     console.log('Service Worker is not supported in this browser.');
   }
-}
+})();
